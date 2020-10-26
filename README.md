3b507c548b12685a11052e1008754d482c4bbb42
speedson <edsoncassiano0@gmail.com>
2020-10-26 08:52:39 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# Bar Chart Race
+## Linguagem de Programação 1 - DIM0120 - 2020.6
+
+This project aims to build a [Bar Chart](https://en.wikipedia.org/wiki/Bar_chart) Race, executed in text mode via terminal.
+
+###### Author Edson Cassiano. October, 2020
---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+int main(int argc, char *argv[]){
+
+// First Scope
+//	Arquitetura gameloop
+//	Gerenciador jogo
+//
+//	inicializarJogo(parametros);
+//
+//	while(verdade){
+//		processar();
+//		atualizar();
+//		imprime();
+//	}
+
+	return 0;
+}
